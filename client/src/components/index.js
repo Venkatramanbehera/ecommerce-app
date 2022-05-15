b2d2f27aebaeb5a75ae5da52a18a42e39749db78
Venkat <venkatramanbehera1@gmail.com>
2022-05-15 13:40:22 +0530

    removing client

--- client/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from '../components/common/Navbar';
-export { default as Input } from '../components/common/Input';
-export { default as Background } from '../components/appspecific/Landing/Background';
--- client/src/views/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Register } from './auth/Register';
-export { default as Login } from './auth/Login';
